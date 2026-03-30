e765615b78347552a560a5cc3a6096743c4958a2
Naebro <sripror0507@gmail.com>
2026-03-30 18:05:26 +0300

    тоже самое что внизу

--- post/res/scroll-to-top.js
+++ post/res/scroll-to-top.js
@@ -1,7 +1,7 @@
 const scrollBtn = document.createElement('button');
 scrollBtn.id = 'scroll-to-top-btn';
 scrollBtn.setAttribute('aria-label', 'Вернуться в начало');
-scrollBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>';
+scrollBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>';
 document.body.appendChild(scrollBtn);
 
 window.addEventListener('scroll', () => {
